e42313b9745968514ebc4a7d0eb789bb48dca9e6
Aryan Regmi <aryan_regmi@protonmail.com>
2024-09-10 12:51:38 -0600

    Fixed ne and eq

--- src/mun/measurement.py
+++ src/mun/measurement.py
@@ -176,18 +176,14 @@ class Measurement[T]:
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Measurement):
             return self.unit == other.unit and self.value == other.value
-        elif isinstance(other, float):
-            return self.value == other
         else:
-            raise TypeError("`other` must be `Measurement` or `float`")
+            return self.value == other
 
     def __ne__(self, other: object) -> bool:
         if isinstance(other, Measurement):
             return self.unit != other.unit or self.value != other.value
-        elif isinstance(other, float):
-            return self.value != other
         else:
-            raise TypeError("`other` must be `Measurement` or `float`")
+            return self.value != other
 
     def __gt__(self, other: Measurement | float) -> bool:
         if isinstance(other, Measurement):
